87e55f6fe1aa4136415a1a55e965ac052bcb78b5
Hunter Lam <hunter.lam@outlook.com>
2025-09-01 22:11:55 +0800

    style: remove button

--- src/components/DiscountSection.tsx
+++ src/components/DiscountSection.tsx
@@ -17,56 +17,65 @@ const DiscountSection: React.FC<DiscountSectionProps> = ({ form }) => {
         {(fields, { add, remove }) => (
           <Space direction="vertical" style={{ width: '100%' }}>
             {fields.map(({ key, name, ...restField }) => (
-              <Space key={key} wrap style={{ marginBottom: 8, width: '100%' }}>
-                <Form.Item
-                  {...restField}
-                  name={[name, "discountOwner"]}
-                  rules={[{ required: true, message: "請選擇優惠提供者" }]}
-                  style={{ marginBottom: 0, minWidth: 120 }}
-                >
-                  <Select 
-                    placeholder="優惠提供者" 
-                    popupMatchSelectWidth={false} 
-                    options={DISCOUNT_ORGANIZERS.map(v => ({ label: v, value: v }))}
-                    style={{ width: 120 }}
-                  />
-                </Form.Item>
-                
-                <Form.Item
-                  {...restField}
-                  name={[name, "discountType"]}
-                  rules={[{ required: true, message: "請選擇優惠類型" }]}
-                  style={{ marginBottom: 0, minWidth: 120 }}
-                >
-                  <Select 
-                    placeholder="優惠類型" 
-                    popupMatchSelectWidth={false} 
-                    options={DISCOUNT_METHODS.map(v => ({ label: v, value: v }))}
-                    style={{ width: 120 }}
-                  />
-                </Form.Item>
-                
-                <Form.Item
-                  {...restField}
-                  name={[name, "discountValue"]}
-                  rules={[{ required: true, message: "請輸入優惠值" }]}
-                  style={{ marginBottom: 0, minWidth: 200 }}
-                >
-                  <DiscountInput 
-                    format={form.getFieldValue(["discount", name, "discountType"])} 
-                  />
-                </Form.Item>
+              <div key={key} style={{ 
+                display: 'flex', 
+                justifyContent: 'space-between', 
+                alignItems: 'flex-start',
+                marginBottom: 8, 
+                width: '100%' 
+              }}>
+                <Space wrap>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "discountOwner"]}
+                    rules={[{ required: true, message: "請選擇優惠提供者" }]}
+                    style={{ marginBottom: 0, minWidth: 120 }}
+                  >
+                    <Select 
+                      placeholder="優惠提供者" 
+                      popupMatchSelectWidth={false} 
+                      options={DISCOUNT_ORGANIZERS.map(v => ({ label: v, value: v }))}
+                      style={{ width: 120 }}
+                    />
+                  </Form.Item>
+                  
+                  <Form.Item
+                    {...restField}
+                    name={[name, "discountType"]}
+                    rules={[{ required: true, message: "請選擇優惠類型" }]}
+                    style={{ marginBottom: 0, minWidth: 120 }}
+                  >
+                    <Select 
+                      placeholder="優惠類型" 
+                      popupMatchSelectWidth={false} 
+                      options={DISCOUNT_METHODS.map(v => ({ label: v, value: v }))}
+                      style={{ width: 120 }}
+                    />
+                  </Form.Item>
+                  
+                  <Form.Item
+                    {...restField}
+                    name={[name, "discountValue"]}
+                    rules={[{ required: true, message: "請輸入優惠值" }]}
+                    style={{ marginBottom: 0, minWidth: 200 }}
+                  >
+                    <DiscountInput 
+                      format={form.getFieldValue(["discount", name, "discountType"])} 
+                    />
+                  </Form.Item>
+                </Space>
                 
                 <MinusCircleOutlined 
                   style={{ 
                     color: token.colorError, 
                     cursor: 'pointer',
                     fontSize: 16,
-                    padding: 4
+                    padding: 4,
+                    marginTop: 6
                   }}
                   onClick={() => remove(name)} 
                 />
-              </Space>
+              </div>
             ))}
             
             <Button 
